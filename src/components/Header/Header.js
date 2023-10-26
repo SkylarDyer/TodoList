0ed2a8a95efb26c5ad5ab7aeef3134b5745a6032
skylarlol <skdyer26@yahoo.com>
2023-10-25 20:52:33 -0500

    first init, added components

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    // add name functionality
+    <header>
+      <h1 className="header__title">Todo List: Stop procrastinating! </h1>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/List/List.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./List.css";
+const List = () => {
+  return (
+    <div className="list">
+      <h2 className="list__title">Todo</h2>
+      <ul>
+        <li>
+          <div>
+            
+          </div>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default List;
